10dfa1ba3ec2d387e01c5397630cbf80f9e0ffd5
Hdwig <85887645+Hdwig@users.noreply.github.com>
2021-11-09 23:38:57 +0300

    Гипербола

--- Lesson_1_math.py
+++ Lesson_1_math.py
@@ -1,8 +1,8 @@
-%matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.linspace(0, 10, 122)
-k1=1
-k2=5
-plt.plot(x, np.cos(k1*x))
-plt.plot(x, np.cos(k2*x))
+
+x = np.linspace(0, 10, 122)
+k1 = 1
+k2 = 5
+plt.plot(x, np.cos(k1 * x))
+plt.plot(x, np.cos(k2 * x))
---
+++ Lesson_3_2.2.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+a = 5
+a2 = a ** 2
+b = 5
+b2 = b ** 2
+xm = []
+xp = []
+yp = []
+ym = []
+for i in np.linspace(-15, 15, 1000):
+    x1 = i
+    xp.append(x1)
+    xm.append(-1 * x1)
+    yp.append(b * math.sqrt((x1 / a) ** 2) - 1)
+    ym.append(-b * math.sqrt((x1 / a) ** 2) - 1)
+plt.plot(xm, yp)
+# plt.plot(xp, yp)
+plt.plot(xm, ym)
+# plt.plot(xp, ym)
+plt.xlabel("y")
+plt.ylabel("x")
