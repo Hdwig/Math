2ebdf277dc8d74318b9c44ecab5161890a6f88da
Hdwig <85887645+Hdwig@users.noreply.github.com>
2021-11-09 22:59:22 +0300

    Эллипс

--- Lesson_3.2.1.py
+++ Lesson_3.2.1.py
@@ -2,24 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-a = 2
+a = 10
 a2 = a ** 2
-b = 3
+b = 2
 b2 = b ** 2
 xm = []
 xp = []
 yp = []
 ym = []
-for i in np.linspace(-2, 2, 1000):
+for i in np.linspace(-10, 10, 1000):
     x1 = i
     xp.append(x1)
-    xm.append(-1 * x1)
     yp.append(math.sqrt(b2 - (x1 ** 2 * b2) / a2))
     ym.append(-math.sqrt(b2 - (x1 ** 2 * b2) / a2))
-plt.plot(xm, yp)
 plt.plot(xp, yp)
-plt.plot(xm, ym)
 plt.plot(xp, ym)
-# plt.xlim([-3, 3])
 plt.xlabel("x")
 plt.ylabel("y")
